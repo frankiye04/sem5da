46221bfdaf5b354df6eee1314855772ebc890717
Vishal V <168668286+frankiye04@users.noreply.github.com>
2025-07-30 21:04:42 +0530

    Ex1.py

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# ex1.py
+import functools
+import pandas as pd
+numbers = [1, 2, 3, 4, 5]
+doubled_numbers = list(map(lambda x: x * 2, numbers))
+sum_of_numbers = functools.reduce(lambda x, y: x + y, numbers)
+print("Original list:", numbers)
+print("Doubled list:", doubled_numbers)
+print("Sum of original list:", sum_of_numbers)
